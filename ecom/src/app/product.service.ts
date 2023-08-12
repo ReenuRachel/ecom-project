5415bbc2aedf64f192c9521fced845fb0e420b2a
ReenuRachel <248719@ust.com>
2023-08-12 15:24:47 +0530

    first commit

---
+++ ecom/src/app/product.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+   URL='http://localhost:8081/product';
+
+  constructor(private http:HttpClient) { }
+
+  fnHeaders()
+  {
+    var jwt=localStorage.getItem("jwt");
+    const httpOptions = {
+      headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          Authorization: 'Bearer ' + jwt
+      })
+  };
+  return httpOptions;
+  }
+
+  fnFindProductsByUser(user:any)
+  {
+    return this.http.post(this.URL+"/user",user,this.fnHeaders());
+  }
+
+  fnGetAllProducts()
+  {
+    return this.http.get(this.URL, this.fnHeaders());
+  }
+
+  fnGetAllProductsByPage(pageNum:number,pageSize:number){
+    return this.http.get(this.URL+`/page?page=${pageNum}&size=${pageSize}`)
+  }
+  findProductById(id:number)
+  {
+    return this.http.get(this.URL+"/"+id, this.fnHeaders());
+  }
+
+  fnAddProduct(product:any)
+  {
+    return this.http.post(this.URL,product);
+  }
+
+  fnUpdateProduct(product:any)
+  {
+    return this.http.put(this.URL+"/"+product.id,product);
+  }
+
+  fnDeleteProduct(id:number)
+  {
+    return this.http.delete(this.URL+"/"+id);
+  }
+}
